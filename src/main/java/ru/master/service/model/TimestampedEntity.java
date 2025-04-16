b4193884a89b972d328c810ef2a2e9d5079ea4ac
BAKHTOVAR <bakhtovar.abdulloev.03@mail.ru>
2025-04-16 21:29:25 +0500

    Добавил сущность для работы с дата создание и обновление запись

---
+++ src/main/java/ru/master/service/model/TimestampedEntity.java
@@ -0,0 +1,28 @@
+package ru.master.service.model;
+
+import jakarta.persistence.MappedSuperclass;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import lombok.experimental.SuperBuilder;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.Instant;
+
+@Getter
+@SuperBuilder
+@MappedSuperclass
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public abstract class TimestampedEntity extends BaseEntity {
+
+    @CreationTimestamp
+    Instant createdAt;
+
+    @UpdateTimestamp
+    Instant updatedAt;
+}
