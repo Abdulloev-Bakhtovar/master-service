8bf24f44cd3f9dea217f7abc4faeac0dd7e0c62a
Abdulloev-Bakhtovar <bakhtovar.abdulloev.03@mail.ru>
2025-04-17 21:00:05 +0500

    Создал dto с общими полями для мастера и клиент

---
+++ src/main/java/ru/master/service/model/dto/BaseProfileDto.java
@@ -0,0 +1,19 @@
+package ru.master.service.model.dto;
+
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+import lombok.experimental.SuperBuilder;
+import ru.master.service.auth.model.dto.TimestampedDto;
+
+@Getter
+@Setter
+@SuperBuilder
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public abstract class BaseProfileDto extends TimestampedDto {
+
+    String firstName;
+    String lastName;
+    String city;
+}
