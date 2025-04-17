5f4c60c73e94b1befd5f30d16ddead98dba0e254
Abdulloev-Bakhtovar <bakhtovar.abdulloev.03@mail.ru>
2025-04-17 15:56:54 +0500

    Добавил метод для генерации код который отправляется на номер телефона

---
+++ src/main/java/ru/master/service/util/CodeGeneratorUtil.java
@@ -0,0 +1,36 @@
+package ru.master.service.util;
+
+import org.springframework.http.HttpStatus;
+import ru.master.service.constants.ErrorMessage;
+import ru.master.service.exception.AppException;
+
+import java.security.SecureRandom;
+
+public class CodeGeneratorUtil {
+
+    private static final SecureRandom secureRandom = new SecureRandom();
+
+    /**
+     * Генерирует числовой код указанной длины
+     * @param length длина генерируемого кода (количество цифр)
+     * @return сгенерированный числовой код
+     * @throws AppException если длина меньше 1
+     */
+    public static String generateNumericCode(int length) throws AppException {
+
+        if (length < 1) {
+            throw new AppException(
+                    ErrorMessage.INVALID_CODE_LENGTH,
+                    HttpStatus.BAD_REQUEST
+            );
+        }
+
+        StringBuilder code = new StringBuilder(length);
+
+        for (int i = 0; i < length; i++) {
+            code.append(secureRandom.nextInt(10));
+        }
+
+        return code.toString();
+    }
+}
