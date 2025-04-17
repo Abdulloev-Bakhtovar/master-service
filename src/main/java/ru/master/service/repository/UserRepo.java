302ca6e4ec4eef578efe1e4bb296bec19f17952a
Abdulloev-Bakhtovar <bakhtovar.abdulloev.03@mail.ru>
2025-04-17 14:23:18 +0500

    Добавил репозитория для User

---
+++ src/main/java/ru/master/service/repository/UserRepo.java
@@ -0,0 +1,11 @@
+package ru.master.service.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import ru.master.service.model.User;
+
+import java.util.UUID;
+
+public interface UserRepo extends JpaRepository<User, UUID> {
+
+    boolean existsByPhoneNumber(String phoneNumber);
+}
