44646531e76adbbb3fcf9f0ce97cd2cbb185bae9
Abdulloev-Bakhtovar <bakhtovar.abdulloev.03@mail.ru>
2025-04-17 21:04:18 +0500

    Создал dto с поля accessToken

---
+++ src/main/java/ru/master/service/auth/model/dto/TokenDto.java
@@ -0,0 +1,15 @@
+package ru.master.service.auth.model.dto;
+
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class TokenDto {
+
+    String accessToken;
+}
