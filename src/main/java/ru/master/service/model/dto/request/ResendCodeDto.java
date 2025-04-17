0d14c694fc5433fc571af70ec7c58e4aa3e5dbd9
Abdulloev-Bakhtovar <bakhtovar.abdulloev.03@mail.ru>
2025-04-17 17:12:07 +0500

    Добавил dto для подтверждения и повторного отправка кода

---
+++ src/main/java/ru/master/service/model/dto/request/ResendCodeDto.java
@@ -0,0 +1,16 @@
+package ru.master.service.model.dto.request;
+
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+import lombok.experimental.SuperBuilder;
+
+@Getter
+@Setter
+@SuperBuilder
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class ResendCodeDto {
+
+    String phoneNumber;
+}
