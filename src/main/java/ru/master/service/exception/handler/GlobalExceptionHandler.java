f9a7d64764ec28ea4fb54fa4ad55707ea6cc7252
Abdulloev-Bakhtovar <bakhtovar.abdulloev.03@mail.ru>
2025-04-17 20:47:53 +0500

    Изменил шаблон вывода

--- src/main/java/ru/master/service/exception/handler/GlobalExceptionHandler.java
+++ src/main/java/ru/master/service/exception/handler/GlobalExceptionHandler.java
@@ -2,21 +2,42 @@ package ru.master.service.exception.handler;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import ru.master.service.exception.AppException;
+
+import java.util.HashMap;
+import java.util.Map;
 
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(AppException.class)
-    public ResponseEntity<String> handleAppException(AppException ex) {
-        return ResponseEntity.status(ex.getStatus()).body(ex.getMessage());
+    public ResponseEntity<Map<String, String>> handleKvadroksException(AppException ex) {
+        Map<String, String> error = new HashMap<>();
+        error.put("message", ex.getMessage());
+
+        return ResponseEntity.status(ex.getStatus()).body(error);
+    }
+
+    @ExceptionHandler(AccessDeniedException.class)
+    @ResponseStatus(HttpStatus.FORBIDDEN)
+    public Map<String, String> handleAccessDeniedException(AccessDeniedException ex) {
+        Map<String, String> error = new HashMap<>();
+        error.put("message", "Access denied: you do not have permission to perform this action.");
+        return error;
     }
 
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<String> handleOtherExceptions(Exception ex) {
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Internal server error");
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public Map<String, String> handleGenericException(Exception ex) {
+        Map<String, String> error = new HashMap<>();
+        error.put("stack trace", ex.getMessage());
+        error.put("message", "An unexpected error occurred. Please try again later.");
+
+        return error;
     }
 }
