42eca9b4a5c59b11216b80b4a136d207fbdae9da
Abdulloev-Bakhtovar <bakhtovar.abdulloev.03@mail.ru>
2025-04-17 17:09:34 +0500

    Добавил метод findByPhoneNumber

--- src/main/java/ru/master/service/repository/UserRepo.java
+++ src/main/java/ru/master/service/repository/UserRepo.java
@@ -3,9 +3,12 @@ package ru.master.service.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.master.service.model.User;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface UserRepo extends JpaRepository<User, UUID> {
 
     boolean existsByPhoneNumber(String phoneNumber);
+
+    Optional<User> findByPhoneNumber(String phoneNumber);
 }
