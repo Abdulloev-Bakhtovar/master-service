91e8cdd9075932a45161e7ebec9300e0dd18b400
Abdulloev-Bakhtovar <bakhtovar.abdulloev.03@mail.ru>
2025-04-17 15:20:00 +0500

    Добавил констант INVALID_CODE_LENGTH

--- src/main/java/ru/master/service/constants/ErrorMessage.java
+++ src/main/java/ru/master/service/constants/ErrorMessage.java
@@ -1,8 +1,8 @@
 package ru.master.service.constants;
 
 public class ErrorMessage {
-
-    public static final String USER_ALREADY_EXISTS = "User with phone number already exists.";
-    public static final String USER_NOT_FOUND = "User not found.";
-    public static final String INVALID_ROLE = "Invalid role provided.";
+    public static final String USER_ALREADY_EXISTS           = "User with phone number already exists.";
+    public static final String USER_NOT_FOUND                = "User not found.";
+    public static final String INVALID_ROLE                  = "Invalid role provided.";
+    public static final String INVALID_CODE_LENGTH           = "Code length must be a positive number.";
 }
