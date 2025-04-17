8c55b208a27593916e5c6b163f7d737e69036e3e
Abdulloev-Bakhtovar <bakhtovar.abdulloev.03@mail.ru>
2025-04-17 21:10:31 +0500

    Добавлены репозитории

---
+++ src/main/java/ru/master/service/repository/ClientProfileRepo.java
@@ -0,0 +1,9 @@
+package ru.master.service.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import ru.master.service.model.ClientProfile;
+
+import java.util.UUID;
+
+public interface ClientProfileRepo extends JpaRepository<ClientProfile, UUID> {
+}
