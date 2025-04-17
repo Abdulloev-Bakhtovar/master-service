b7e04d690e9baf45054f3b1e741f3d7991b357d5
Abdulloev-Bakhtovar <bakhtovar.abdulloev.03@mail.ru>
2025-04-17 20:54:13 +0500

    Переместил UserRepo на другой пакет

--- src/main/java/ru/master/service/auth/repository/UserRepo.java
+++ src/main/java/ru/master/service/auth/repository/UserRepo.java
@@ -1,7 +1,7 @@
-package ru.master.service.repository;
+package ru.master.service.auth.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import ru.master.service.model.User;
+import ru.master.service.auth.model.User;
 
 import java.util.Optional;
 import java.util.UUID;
