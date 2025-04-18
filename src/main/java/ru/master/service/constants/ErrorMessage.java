691f1920932a5ac17a996b37fc534ebd99963859
Abdulloev-Bakhtovar <bakhtovar.abdulloev.03@mail.ru>
2025-04-18 14:41:46 +0500

    Добавил новые типы ошибки

--- src/main/java/ru/master/service/constants/ErrorMessage.java
+++ src/main/java/ru/master/service/constants/ErrorMessage.java
@@ -1,14 +1,28 @@
 package ru.master.service.constants;
 
 public class ErrorMessage {
+    // User related
+    public static final String USER_ALREADY_EXISTS = "User with phone number already exists.";
+    public static final String USER_NOT_FOUND = "User not found.";
+    public static final String USER_NOT_AUTHENTICATED = "User is not authenticated";
+    public static final String USER_NOT_VERIFIED = "User phone number is not verified";
+    public static final String USER_ALREADY_VERIFIED = "User is already verified.";
 
-    public static final String USER_ALREADY_EXISTS           = "User with phone number already exists.";
-    public static final String USER_NOT_FOUND                = "User not found.";
-    public static final String INVALID_ROLE                  = "Invalid role provided.";
-    public static final String INVALID_CODE_LENGTH           = "Code length must be a positive number.";
-    public static final String CODE_EXPIRED_OR_NOT_FOUND     = "Verification code expired or not found.";
-    public static final String INVALID_VERIFICATION_CODE     = "Invalid verification code.";
-    public static final String USER_ALREADY_VERIFIED         = "User is already verified.";
-    public static final String ASSIGN_ADMIN_ROLE_FORBIDDEN   = "Only administrators can assign the ADMIN role";
-    public static final String USER_NOT_VERIFIED             = "User phone number is not verified";
+    // Role related
+    public static final String INVALID_ROLE = "Invalid role provided.";
+    public static final String ASSIGN_ADMIN_ROLE_FORBIDDEN = "Only administrators can assign the ADMIN role";
+
+    // Verification related
+    public static final String INVALID_CODE_LENGTH = "Code length must be a positive number.";
+    public static final String CODE_EXPIRED_OR_NOT_FOUND = "Verification code expired or not found.";
+    public static final String INVALID_VERIFICATION_CODE = "Invalid verification code.";
+
+    // JWT related
+    public static final String REFRESH_TOKEN_NOT_FOUND = "Refresh token not found in cookies";
+    public static final String TOKEN_BLACKLISTED = "Token blacklisted";
+    public static final String TOKEN_REVOKED = "Token has been revoked";
+    public static final String INVALID_TOKEN_SIGNATURE = "Invalid token signature";
+    public static final String INVALID_TOKEN_TYPE = "Invalid token type";
+    public static final String ONLY_ACCESS_TOKENS_ALLOWED = "Only access tokens are allowed";
+    public static final String TOKEN_EXPIRED = "Token expired";
 }
