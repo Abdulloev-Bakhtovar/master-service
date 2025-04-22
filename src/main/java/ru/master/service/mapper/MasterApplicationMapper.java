07436b321d381d49c8df0ac624b7e6f52214094a
Abdulloev-Bakhtovar <bakhtovar.abdulloev.03@mail.ru>
2025-04-22 18:23:24 +0500

    Добавил маппер для MasterApplication

---
+++ src/main/java/ru/master/service/mapper/MasterApplicationMapper.java
@@ -0,0 +1,12 @@
+package ru.master.service.mapper;
+
+import ru.master.service.auth.model.User;
+import ru.master.service.constants.VerificationStatus;
+import ru.master.service.model.MasterApplication;
+
+public interface MasterApplicationMapper {
+
+    MasterApplication toEntity(User user, VerificationStatus verificationStatus);
+
+    void toEntity(User admin, User user, VerificationStatus verificationStatus, MasterApplication existsApplication);
+}
