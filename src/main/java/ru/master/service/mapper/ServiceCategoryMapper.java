fd394295210e821e163953e40917c5f878d09bcd
Abdulloev-Bakhtovar <bakhtovar.abdulloev.03@mail.ru>
2025-04-22 18:10:03 +0500

    Добавил маппер с entity на dto

--- src/main/java/ru/master/service/mapper/ServiceCategoryMapper.java
+++ src/main/java/ru/master/service/mapper/ServiceCategoryMapper.java
@@ -1,9 +1,11 @@
 package ru.master.service.mapper;
 
+import ru.master.service.model.MasterSubService;
 import ru.master.service.model.ServiceCategory;
 import ru.master.service.model.SubServiceCategory;
 import ru.master.service.model.dto.ServiceCategoryDto;
 
+import java.util.List;
 import java.util.Set;
 
 public interface ServiceCategoryMapper {
@@ -11,4 +13,6 @@ public interface ServiceCategoryMapper {
     ServiceCategory toEntity(ServiceCategoryDto dto, Set<SubServiceCategory> subServices);
 
     ServiceCategoryDto toDto(ServiceCategory entity);
+
+    List<ServiceCategoryDto> toDtoList(List<MasterSubService> masterSubServices);
 }
--- src/main/java/ru/master/service/mapper/impl/ServiceCategoryMapperImpl.java
+++ src/main/java/ru/master/service/mapper/impl/ServiceCategoryMapperImpl.java
@@ -4,10 +4,12 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import ru.master.service.mapper.ServiceCategoryMapper;
 import ru.master.service.mapper.SubServiceCategoryMapper;
+import ru.master.service.model.MasterSubService;
 import ru.master.service.model.ServiceCategory;
 import ru.master.service.model.SubServiceCategory;
 import ru.master.service.model.dto.ServiceCategoryDto;
 
+import java.util.List;
 import java.util.Set;
 
 @Component
@@ -35,5 +37,23 @@ public class ServiceCategoryMapperImpl implements ServiceCategoryMapper {
                 .name(entity.getName())
                 .subServiceCategoryDtos(subServiceMapper.toDtoList(entity.getSubServices()))
                 .build();
+    }
+
+    private ServiceCategoryDto toDto(MasterSubService entity) {
+        if (entity == null) return null;
+
+        return ServiceCategoryDto.builder()
+                .id(entity.getServiceCategory().getId())
+                .name(entity.getServiceCategory().getName())
+                .subServiceCategoryDtos(subServiceMapper.toDtoList(entity.getServiceCategory().getSubServices()))
+                .build();
+    }
+
+    @Override
+    public List<ServiceCategoryDto> toDtoList(List<MasterSubService> masterSubServices) {
+
+        return masterSubServices.stream()
+                .map(this::toDto)
+                .toList();
     }
 }
