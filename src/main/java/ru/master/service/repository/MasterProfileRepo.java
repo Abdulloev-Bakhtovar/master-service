31a3eb7ec33099e80ba387e3bac1f003eb2365c9
Abdulloev-Bakhtovar <bakhtovar.abdulloev.03@mail.ru>
2025-04-22 18:08:42 +0500

    добавил метод findByUserId

--- src/main/java/ru/master/service/repository/MasterProfileRepo.java
+++ src/main/java/ru/master/service/repository/MasterProfileRepo.java
@@ -3,9 +3,12 @@ package ru.master.service.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.master.service.model.MasterProfile;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface MasterProfileRepo extends JpaRepository<MasterProfile, UUID> {
 
     boolean existsByUserId(UUID id);
+
+    Optional<MasterProfile> findByUserId(UUID id);
 }
