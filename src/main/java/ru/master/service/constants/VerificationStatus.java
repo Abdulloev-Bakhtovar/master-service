2d043c12db9b3807d1e6ba4c210824a211e83735
Abdulloev-Bakhtovar <bakhtovar.abdulloev.03@mail.ru>
2025-04-22 18:02:46 +0500

    Удалил лишение статусы

--- src/main/java/ru/master/service/constants/VerificationStatus.java
+++ src/main/java/ru/master/service/constants/VerificationStatus.java
@@ -12,19 +12,15 @@ public enum VerificationStatus {
     PHONE_VERIFIED("Номер подтвержден"),
 
     // Статусы заполнения информации
-    INFO_NOT_ENTERED("Информация не введена"),
     INFO_ENTERED("Информация введена"),
 
     // Статусы документов
-    DOCS_NOT_UPLOADED("Документы не загружены"),
-    DOCS_PARTIALLY_UPLOADED("Документы загружены частично"),
-    DOCS_UPLOADED("Документы загружены"),
+   DOCS_UPLOADED("Документы загружены"),
 
     // Статусы проверки
     UNDER_REVIEW("На проверке"),
     APPROVED("Проверка пройдена"),
-    REJECTED("Отказано"),
-    NEEDS_CORRECTION("Требуются исправления");
+    REJECTED("Отказано");
 
     private final String displayName;
 }
