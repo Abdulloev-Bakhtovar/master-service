fb28c14c2b9739cc97a577479c0cf51714fe6e88
Abdulloev-Bakhtovar <bakhtovar.abdulloev.03@mail.ru>
2025-04-27 12:40:02 +0500

    Добавил метод для получения картинки категория услуг используя его Id

--- src/main/java/ru/master/service/contoller/ServiceCategoryController.java
+++ src/main/java/ru/master/service/contoller/ServiceCategoryController.java
@@ -3,12 +3,16 @@ package ru.master.service.contoller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import ru.master.service.constants.DocumentType;
 import ru.master.service.model.dto.ServiceCategoryDto;
 import ru.master.service.model.dto.request.ServiceCategoryReqDto;
+import ru.master.service.service.FileStorageService;
 import ru.master.service.service.ServiceCategoryService;
 
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/service-categories")
@@ -16,6 +20,7 @@ import java.util.List;
 public class ServiceCategoryController {
 
     private final ServiceCategoryService serviceCategoryService;
+    private final FileStorageService fileStorageService;
 
     @GetMapping
     public List<ServiceCategoryDto> getAll(@RequestParam(required = false) String name) {
@@ -31,5 +36,15 @@ public class ServiceCategoryController {
     @ResponseStatus(HttpStatus.CREATED)
     public void create(@ModelAttribute ServiceCategoryReqDto dto) throws Exception {
         serviceCategoryService.create(dto);
+    }
+
+    @GetMapping("/{id}/image")
+    public ResponseEntity<byte[]> getImage(@PathVariable UUID id) {
+        byte[] imageData = fileStorageService.loadFile(DocumentType.SERVICE_CATEGORY_PHOTO, id);
+        MediaType mediaType = fileStorageService.getMediaType(DocumentType.SERVICE_CATEGORY_PHOTO, id);
+
+        return ResponseEntity.ok()
+                .contentType(mediaType)
+                .body(imageData);
     }
 }
