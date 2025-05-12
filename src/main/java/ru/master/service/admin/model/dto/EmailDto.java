b2780243d54ddfd2b92834f7fbc0c502a2f7adf6
Abdulloev-Bakhtovar <bakhtovar.abdulloev.03@mail.ru>
2025-05-12 16:28:02 +0500

    Исправил ошибку: не правильный путь добавил в public-endpoints для сброса пароля

--- src/main/java/ru/master/service/admin/model/dto/EmailDto.java
+++ src/main/java/ru/master/service/admin/model/dto/EmailDto.java
@@ -2,11 +2,12 @@ package ru.master.service.admin.model.dto;
 
 import lombok.*;
 import lombok.experimental.FieldDefaults;
+import lombok.experimental.SuperBuilder;
 
 @Getter
 @Setter
-@Builder
 @ToString
+@SuperBuilder
 @NoArgsConstructor
 @AllArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE)
